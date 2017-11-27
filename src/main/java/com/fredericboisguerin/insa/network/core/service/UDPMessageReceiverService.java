8f04e0ca940dec11e1d22e74acdbc7c0cc2120f9
Pierrot31 <plaurens@etud.insa-toulouse.fr>
2017-11-27 16:40:05 +0100

    TD4 UDP Terminé: messages envoyés et reçus.

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,47 @@
 package com.fredericboisguerin.insa.network.core.service;
 
-public class UDPMessageReceiverService implements MessageReceiverService {
+import javax.naming.ldap.StartTlsRequest;
+import javax.sql.DataSource;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+
+public class UDPMessageReceiverService implements MessageReceiverService,Runnable {
+
+    private byte[] dataReception;
+    private DatagramSocket datagramSocket;
+    private DatagramPacket datagramPacket;
+    private IncomingMessageListener monincomingMessageListener;
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+
+
+        byte[] buffer = new byte[100];
+        this.dataReception = new byte[100];
+
+
+        this.datagramSocket = new DatagramSocket(port);
+        this.datagramPacket = new DatagramPacket(buffer, 100);
+        this.monincomingMessageListener = incomingMessageListener;
+        new Thread(()-> run()).start();
+
+        //throw new UnsupportedOperationException();
+    }
+    public void affichagemessage(){
+        this.dataReception = datagramPacket.getData();
+        String chainerecue = new String(dataReception);
+        monincomingMessageListener.onNewIncomingMessage(chainerecue);
+    }
+    @Override
+    public void run() {
+        boolean threadreceptionactive = true;
+        while (threadreceptionactive) {
+            try {
+                this.datagramSocket.receive(this.datagramPacket);
+                this.affichagemessage();
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
     }
 }
